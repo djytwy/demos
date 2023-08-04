ebb1940f1c59e416174a13b95c83264fdcbe97c2
djytwy <676534074@qq.com>
2023-08-04 17:51:57 +0800

    修改引用错误

--- src/components/useContextDemo/errorCode.tsx
+++ src/components/useContextDemo/errorCode.tsx
@@ -1,90 +1,92 @@
 import {
-    createContext,
-    type Dispatch,
-    type ReactNode,
-    type SetStateAction,
-    useContext,
-    useState,
-    PropsWithChildren,
-  } from 'react';
-  
-  interface dataProps {
-    account: string;
-    setAccount: Dispatch<SetStateAction<string>>;
-  }
-  
-  const Ctx = createContext<dataProps>({
-    account: '',
-    setAccount: () => {},
-  });
-  
-  const Provider = ({ children }: PropsWithChildren) => {
-    const [account, setAccount] = useState('');
-  
-    return (
-      <Ctx.Provider
-        value={{
-          account,
-          setAccount,
-        }}
-      >
-        {children}
-      </Ctx.Provider>
-    );
-  };
-  
-  const useSignInContext = () => useContext(Ctx);
-  
-  export { Provider as SignInProvider, useSignInContext };
-  
-  function ErrorCode() {
-    // 错误的使用方式，因为不能在Provider外部使用useSignInContext函数，会导致调用失效
-    const { account, setAccount } = useSignInContext();
-  
-    return (
-      <>
-        <Provider>
-          <div>
-            <button
-              onClick={() => {
-                setAccount('recover');
-                console.log(setAccount);
-  
-                console.log('1232');
-              }}
-            >
-              {' '}
-              click
-            </button>
-          </div>
-        </Provider>
-      </>
-    );
-  }
-  
-function App() {
-    const { account, setAccount } = useSignInContext();
-    return (
-        <div>
-            {account}
-            <button
-              onClick={() => {
-                setAccount('recover');
-                console.log(setAccount);
-  
-                console.log('1232');
-              }}
-            >
-              {' '}
-              click
-            </button>
-        </div>
-    )
+  createContext,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+  useContext,
+  useState,
+  PropsWithChildren,
+} from 'react';
 
+interface dataProps {
+  account: string;
+  setAccount: Dispatch<SetStateAction<string>>;
 }
 
-function Fixed() {
+const Ctx = createContext<dataProps>({
+  account: '',
+  setAccount: () => {},
+});
+
+const Provider = ({ children }: PropsWithChildren) => {
+  const [account, setAccount] = useState('');
+
+  return (
+    <Ctx.Provider
+      value={{
+        account,
+        setAccount,
+      }}
+    >
+      {children}
+    </Ctx.Provider>
+  );
+};
+
+const useSignInContext = () => useContext(Ctx);
+
+export { Provider as SignInProvider, useSignInContext };
+
+function ErrorCode() {
+  // 错误的使用方式，因为不能在Provider外部使用useSignInContext函数，会导致调用失效
+  const { account, setAccount } = useSignInContext();
+
+  return (
+    <>
+      <Provider>
+        <div>
+          <button
+            onClick={() => {
+              setAccount('recover');
+              console.log(setAccount);
+
+              console.log('1232');
+            }}
+          >
+            {' '}
+            click
+          </button>
+        </div>
+      </Provider>
+    </>
+  );
+}
+  
+function App() {
+  const { account, setAccount } = useSignInContext();
+  return (
+    <div>
+      {account}
+      <button
+        onClick={() => {
+          setAccount('recover');
+          console.log(setAccount);
+
+          console.log('1232');
+        }}
+      >
+        {' '}
+        click
+      </button>
+    </div>
+  )
+}
+
+// 正确的使用方式：
+export default function Fixed() {
+  return (
     <Provider>
-        <App></App>
+      <App />
     </Provider>
+  );
 }
